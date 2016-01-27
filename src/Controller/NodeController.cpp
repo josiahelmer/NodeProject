e041543914e7f1af7ec0eaf8678248484a23161a
josiahelmer <jelm9149@740s-pl-112.local>
2016-01-27 13:06:45 -0700

    Start up

---
+++ src/Controller/NodeController.cpp
@@ -0,0 +1,22 @@
+/*
+ * NodeController.cpp
+ *
+ *  Created on: Jan 27, 2016
+ *      Author: jelm9149
+ */
+
+#include "NodeController.h"
+
+NodeController::NodeController() {
+	// TODO Auto-generated constructor stub
+
+}
+
+NodeController::~NodeController() {
+	// TODO Auto-generated destructor stub
+}
+
+void NodeController :: start();
+{
+
+}
