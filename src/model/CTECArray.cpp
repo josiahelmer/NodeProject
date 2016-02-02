cd2f2f91fbb0814343b45479830837c09b075327
josiahelmer <jelm9149@740s-pl-112.local>
2016-02-02 13:29:47 -0700

    more CTEC array
    Traversing the array to get the value

--- src/model/CTECArray.cpp
+++ src/model/CTECArray.cpp
@@ -6,13 +6,58 @@
  */
 
 #include "CTECArray.h"
+using namespace std;
 
-CTECArray::CTECArray() {
-	// TODO Auto-generated constructor stub
+template <class Type>
+CTECArray<Type>::CTECArray(int size)
+{
+	this->size = size;
 
 }
 
-CTECArray::~CTECArray() {
-	// TODO Auto-generated destructor stub
+template <class Type>
+CTECArray<Type>::~CTECArray()
+{
+
+}
+
+template<class Type>
+int CTECArray<Type>::getSize()
+{
+	return this-> size;
+}
+
+template <class Type>
+Type* CTECArray<Type> :: get(int position)
+{
+	// We need to do bounds checking so we do not crash the program
+	if(position >= size || position < 0)
+	{
+		//I am out of bounds nd need to do something about it.
+		cerr << "position value is out og bounds" << endl;
+		return nullptr;
+	}
+	else
+	{
+		// I am in bounds
+		ArrayNode<Type> * current = head;
+		for(int spot = 0; spot <= position; spot++)
+		{
+			if(spot != position)
+			{
+				current = current->getNext();
+			}
+			else
+			{
+
+			}
+		}
+	}
+}
+
+template <class Type>
+void CTECArray<Type> ::set(int position, Type value)
+{
+
 }
 
